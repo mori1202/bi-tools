117bdf27d8ad3415ab48fc5215defa877a55ea2e
il10077 <t.mori@interlink-j.com>
2019-03-08 02:47:32 +0000

    直接記述しているjs,cssをそれぞれのディレクティブに移設

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -4,8 +4,6 @@
 <head>
     <meta charset="utf-8">
     <title>{{{isset($title) ? $title : 'Hello'}}}</title>
-    <link href="{{{asset('/assets/css/style.css')}}}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     @yield('addCss')
 </head>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
